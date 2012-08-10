55c5573d367cbce86ad29a4e5f95e4a4724d7912
cesar <cesar@cesarubuntu>
2012-08-10 11:34:41 +0200

    More things working! Trying collada

--- media/game/Cuboid.js
+++ media/game/Cuboid.js
@@ -16,37 +16,63 @@ CUBOID.create = function(opts){
 	var cub = {show:{}, actions:{}}
 	
 	var createModelCuboid = function(model){
-		var loader = new THREE.JSONLoader();
-		loader.load(model, createCuboid, staticurl+'/'+opts.type+'/texts');
+		var loader = new THREE.ColladaLoader();
+		loader.convertUpAxis = true;
+		loader.load(model, createCuboid)//, staticurl+'/'+opts.type+'/texts');
 	};
 	var createFaceCuboid = function(){
 		var texture = THREE.ImageUtils.loadTexture(staticurl+'/texts/'+opts.text);
 	    texture.wrapT = texture.wrapS = THREE.RepeatWrapping;
 		cub.show = new THREE.Mesh(
 			    new THREE.CubeGeometry(opts.size[0], opts.size[1], opts.size[2]),
-			    new THREE.MeshLambertMaterial({map:texture})
-			);
+			    new THREE.MeshLambertMaterial({map:texture, ambient:0x444444}));
 		opts.scene.add( cub.show );
+		cub.show.castShadow = true;
+		cub.show.receiveShadow = true;
 		cub.show.position.set(opts.pos.x, opts.pos.y, opts.pos.z);
+		cub.type = 'floor'
 	}
 	
 	function createCuboid(geometry){
-		//var object = tQuery.createCube().addTo(opts.world);
-		cub.show = THREE.SceneUtils.createMultiMaterialObject(geometry,geometry.materials)
-		//cub.show.castShadow = true;
-		//cub.show.receiveShadow = true;
+		var dae = geometry.scene;
+		var skin = geometry.skins[0];
+		dae.rotation.x = -Math.PI/2
+		dae.updateMatrix();
+		cub.show = dae
+		
+		//cub.show = THREE.SceneUtils.createMultiMaterialObject(geometry,geometry.materials)
+		
+		cub.show.size = opts.size
+			
 		opts.scene.add(cub.show)
 		cub.show.position.set(opts.pos.x, opts.pos.y, opts.pos.z);	
-		if(opts.focus){ opts.focus.lookAt(cub.show.position); cub.show.add(opts.focus);} 
+		
+		cub.show.animations = geometry.animations;
+		$.each(cub.show.animations, function(i,v){
+			THREE.AnimationHandler.add(v)
+			geometry.animations[i] = new THREE.Animation(v.node, v.data)
+			geometry.animations[i].timeScale = 1;
+		})
+		
+		if(opts.focus){
+			opts.focus.lookAt(cub.show.position); 
+		}
 		CUBOID.add(cub)
 		for (i in cub.show.children){ 
 			cub.show.children[i].prnt = true; 
 			opts.objects.push(cub.show.children[i])
+			cub.show.children[i].castShadow = true;
+			cub.show.children[i].receiveShadow = true;
 		}
-		
+
+		if(opts.actions.mousedown)
+			CUBOID.createAction(opts.actions.mousedown, geometry, cub)
+		else{
+			cub.update = function(){}
+		}
 		cub.actions.mousedown = function(){alert('TADA!')};
-		cub.actions.mouseup = function(){alert('PACHIN!')};
-		cub.actions.mousemove = function(){alert('aha')}
+		cub.actions.mouseup = function(){};
+		cub.actions.mousemove = function(){}
 	}
 	
 	if(opts.model) createModelCuboid(staticurl+'/'+opts.type+'/'+opts.geometry);
@@ -58,6 +84,7 @@ CUBOID.create = function(opts){
 		cub.actions.mousemove = function(){};
 		
 		cub.show.prnt = false
+		cub.update = function(){}
 		CUBOID.add(cub)
 		opts.objects.push(cub.show)
 	}
@@ -65,7 +92,57 @@ CUBOID.create = function(opts){
 	return function(){return cub};
 };
 
+CUBOID.createAction = function(str, geometry, cub){
+	var patt = /\-[a-z]+/
+	var action = str.slice(0,str.search(patt))
+	var type = str.slice(str.search(patt)+1)
+	switch(action){
+	case 'animate':
+		var lastTimestamp = Date.now()
+		cub.update = function(time){
+			var frameTime = ( time - lastTimestamp ) * 0.001; // seconds
+			cub.show.animations[0].update( frameTime );
+			lastTimestamp = time;
+			}
+		break;
+	}
+}
+
+
 CUBOID.event = function(type, obj3D){
 	var cub = CUBOID.getByForm(obj3D.id);
 	cub.actions[type]()
+}
+
+CUBOID.checkCollisions = function(obj){
+	// Using a little trigonometry calculate a point in
+	// space 1000 units in front of our character
+	var deltaX = Math.floor(Math.sin(obj.rotation.y));
+	var deltaZ = Math.floor(Math.cos(obj.rotation.y));
+
+	// Fire a ray from the centre point of our character to the
+	// collision focus point
+	var npos1 = obj.position.clone();
+	npos1.y += 0.5;
+	var ray = new THREE.Ray(npos1,  new THREE.Vector3(deltaX, 0, deltaZ));
+	var npos2 = obj.position.clone()
+	npos2.y += 5
+	var ray2 = new THREE.Ray(npos2,  new THREE.Vector3(deltaX, 0, deltaZ));
+	var intersects = ray.intersectObjects( objects );
+	var intersects2 = ray2.intersectObjects( objects );
+
+	
+	if(intersects.length)
+		if(intersects[0].distance<=1){
+			obj.position.x -= deltaX; camera.position.x -= deltaX;
+			obj.position.z -= deltaZ; camera.position.z -= deltaZ;
+			return true;
+		}
+	if(intersects2.length)
+		if(intersects2[0].distance<=1) {
+			obj.position.x -= deltaX; camera.position.x -= deltaX;
+			obj.position.z -= deltaZ; camera.position.z -= deltaZ;
+			return true;
+		}
+	
 }
--- media/game/game3d.js
+++ media/game/game3d.js
@@ -1,14 +1,27 @@
 staticurl = '/static';
-var pj, onMove=false, domEvent, projector = new THREE.Projector;
+var pj, onMove=false, domEvent, projector = new THREE.Projector, objects = [], 
+mouseClickPos, animations = [], animHandler = THREE.AnimationHandler, collisions;
+;
 $(document).ready(function(){
 	var renderer = new THREE.WebGLRenderer({antialias:true});
 	renderer.setSize( document.body.clientWidth, document.body.clientHeight );
 	document.body.appendChild(renderer.domElement);
 	renderer.setClearColorHex(0xEEEEEE, 1.0);
     renderer.clear();
-    //renderer.shadowMapEnabled = true;
+    renderer.shadowMapEnabled = true;
+    renderer.shadowMapSoft = true;
 
-	scene = new THREE.Scene();
+    stats = new Stats();
+	stats.domElement.style.position	= 'absolute';
+	stats.domElement.style.bottom	= '0px';
+	document.body.appendChild( stats.domElement );
+	
+	$(document).add($('<div style="position:absolute; bottom:80px; id="coll"/>'))
+    
+	Physijs.scripts.worker = staticurl+'/game/physijs_worker.js';
+	Physijs.scripts.ammo = staticurl+'/game/ammo.js';
+	
+	scene = new THREE.Scene;
 
 	camera = new THREE.PerspectiveCamera(
 	    35,         // Field of view
@@ -16,9 +29,8 @@ $(document).ready(function(){
 	    .1,         // Near
 	    10000       // Far
 	);
-	camera.position.set(0, 34, -30);
+	camera.position.set(0, 34, -26);
 	scene.add(camera);
-	var objects = [];
 
 	$.getJSON(staticurl+'/maps/map3d.json', function(data){
 		var floors = data.floor, entities = data.entity, actors = data.actors,
@@ -32,14 +44,15 @@ $(document).ready(function(){
 		
 		$.each(entities, function(i,v){
 			CUBOID.create({model:true, objects:objects, scene:scene, type:'entity', 
-				geometry:v.geometry, pos:new THREE.Vector3(v.pos[0],v.pos[1],v.pos[2]), size:v.size})
+				geometry:v.geometry, pos:new THREE.Vector3(v.pos[0],v.pos[1],v.pos[2]), 
+				size:v.size, actions:v.actions})
 		});
 		
 		$.each(actors, function(i,v){
 			if(v.focus) v.focus = camera
-			var obj= CUBOID.create({model:true, objects:objects, scene:scene, type:'actor', 
+			var obj = CUBOID.create({model:true, objects:objects, scene:scene, type:'actor', 
 				geometry:v.geometry, pos:new THREE.Vector3(v.pos[0],v.pos[1],v.pos[2]), 
-				size:v.size, focus:v.focus})
+				size:v.size, focus:v.focus, actions:v.actions})
 			if(v.focus) pj = obj
 		});
 		
@@ -47,45 +60,48 @@ $(document).ready(function(){
 		scene.add(ambientlight);
 			
 		var pos = directionallight.direction;
-		directionallight = new THREE.DirectionalLight(directionallight.color);
+		directionallight = new THREE.DirectionalLight(directionallight.color, 1.5);
 		directionallight.position.set(pos[0], pos[1], pos[2]);
 		scene.add(directionallight);
-		
+		directionallight.castShadow = true;
+		directionallight.shadowDarkness = 0.5;
+		directionallight.shadowCameraLeft = -60;
+		directionallight.shadowCameraTop = -60;
+		directionallight.shadowCameraRight = 60;
+		directionallight.shadowCameraBottom = 60;
+		directionallight.shadowCameraNear = 1;
+		directionallight.shadowCameraFar = 500;
+		directionallight.shadowBias = -.001
+		directionallight.shadowMapWidth = directionallight.shadowMapHeight = 2048;
+		directionallight.shadowDarkness = .7;
 	});
-//	var floor = CUBOID.create({model:false, objects:objects, scene:scene, type:'floor', text:'t.jpg', pos:new THREE.Vector3(0,0,0), size:{x:25,y:0.1,z:50}})
-
-	/*var light = new THREE.DirectionalLight( 0xFFFFFF );
-	light.position.set( 0, 17, -15 );
-	//light.castShadow = true;
 	
-	var olight = new THREE.AmbientLight(0xFFFFFF);
-	
-	scene.add( light );
-	scene.add(olight);
-	
-	domEvent = new THREEx.DomEvent(camera, document.body)
-	
-	var objects = [];
-	
-	var actorsecundariobob = CUBOID.create({model:true, objects:objects, scene:scene, type:'actor', geometry:'centinela.js', pos:new THREE.Vector3()});
-	var cubo = CUBOID.create({model:true, objects:objects, scene:scene, type:'entity', geometry:'cubo2.js', pos:new THREE.Vector3(-2,0, -4)})
-	var puerta = CUBOID.create({model:true, objects:objects, scene:scene, type:'entity', geometry:'puerta2.js', pos:new THREE.Vector3(-10,0, 10)})
-
-	pj = CUBOID.create({model:true, objects:objects, scene:scene, type:'actor', geometry:'prota.js', pos:new THREE.Vector3(1,0,2), focus:camera})
-	*/
 	mousePos = {x:0,y:0};
 	$(document).mousemove(function(e){ 
 		mousePos.x = e.pageX;
 		mousePos.y = e.pageY;
 	});
 	THREEx.WindowResize(renderer, camera);
-	var render = function(){
-		 renderer.render(scene, camera);
-		 requestAnimationFrame(render);
-		 
-		 if(onMove) moveCharToMouse()
+
+	var render = function(time){
+		$.each(CUBOID.list, function(i, cub){
+			cub.update(time)
+		});
+		
+		if(pj){ if(pj().show.children){
+			var pjj = pj().show;
+			if(CUBOID.checkCollisions(pjj)){
+				//onMove= false
+			}
+		}}
+		renderer.render(scene, camera);
+		stats.update()
+		if(onMove) moveCharToMouse()
+		
+		requestAnimationFrame(render);
 	}
-	render();
+	requestAnimationFrame(render);
+
 	$(document).mousedown(onDocumentEvent('mousedown'));
 	$(document).mouseup(onDocumentEvent('mouseup'));
 	$(document).mousemove(onDocumentEvent('mousemove'));
@@ -97,14 +113,17 @@ function onDocumentEvent(type) {return function( event ) {
 	    var ray = projector.pickingRay(vector.clone(), camera);
 	    var intersects = ray.intersectObjects( objects );
 
-	    if ( intersects.length > 0 ){
+	    if ( intersects.length > 0 ) {
+		    mouseClickPos = intersects[0].point;
 	    	if (intersects[0].object.prnt)
 	    		CUBOID.event(type, intersects[0].object.parent)
 	        else
 	        	CUBOID.event(type, intersects[0].object)
 	    }
 	}}; 
+	
 });
+
 function toScreenXY ( position, camera, jqdiv ) {
     var pos = position.clone();
     projScreenMat = new THREE.Matrix4();
@@ -114,9 +133,18 @@ function toScreenXY ( position, camera, jqdiv ) {
     return { x: ( pos.x + 1 ) * jqdiv.width() / 2 + jqdiv.offset().left,
          y: ( - pos.y + 1) * jqdiv.height() / 2 + jqdiv.offset().top };
 }
-function moveCharToMouse(){
+function moveCharToMouse(position){
 	var pjj = pj().show
 	var pjPos = toScreenXY(new THREE.Vector3(pjj.position.x,pjj.position.y, pjj.position.z), camera, $(document.body));
-	pjj.translateX((pjPos.x-mousePos.x)/1000); pjj.translateZ((pjPos.y-mousePos.y)/1000);
-//	pjj.rotation.y = Math.atan((pjPos.y-mousePos.y)/(pjPos.x-mousePos.x))
+
+	/*pjj.eulerOrder = 'YXZ'
+	pjj.lookAt(new THREE.Vector3(mouseClickPos.x, pjj.position.y, mouseClickPos.z ))
+	pjj.rotation.x = -Math.PI/2
+	*/
+	
+	pjj.position.x += (pjPos.x-mousePos.x)/1000;
+	pjj.position.z += (pjPos.y-mousePos.y)/1000;
+	
+	camera.position.x += (pjPos.x-mousePos.x)/1000;
+	camera.position.z += (pjPos.y-mousePos.y)/1000;
 }
